c610969a80d60e1a4d871555733122d4a5ff4828
FranAmbrogioItec <f.ambrogio@itecriocuarto.org.ar>
2025-08-25 07:33:45 -0300

    Add CI/CD configuration

--- test.js
+++ test.js
@@ -1,10 +1,1 @@
-// test.js
 console.log("Test successfully");
-
-// Puedes agregar más validaciones simples si quieres
-function simpleTest() {
-    return "Test completed";
-}
-
-// Ejecutar la función y mostrar resultado
-console.log(simpleTest());
