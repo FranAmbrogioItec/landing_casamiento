4727e9b98f325bbb32f326dad8f4296a054c74b6
Francisco Ambrogio <f.ambrogio@itecriocuarto.org.ar>
2025-08-25 06:43:52 -0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+// test.js
+console.log("Test successfully");
+
+// Puedes agregar más validaciones simples si quieres
+function simpleTest() {
+    return "Test completed";
+}
+
+// Ejecutar la función y mostrar resultado
+console.log(simpleTest());
